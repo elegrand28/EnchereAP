1e589090fd23d2a4b8625ad0488f273582105ea7
jules thivend <Julesthivend@gmail.com>
2022-11-29 17:38:32 +0100

    'Enchere classique'

--- ApS2/VueModele/PageChoixEnchereVuesModele.cs
+++ ApS2/VueModele/PageChoixEnchereVuesModele.cs
@@ -1,5 +1,6 @@
 ﻿using ApS2.Modele;
 using ApS2.Services;
+using ApS2.Vue;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -9,19 +10,29 @@ using System.Threading.Tasks;
 
 namespace ApS2.VueModele
 {
-    public class PageChoixEnchereVuesModele :BaseVueModele
+    public class PageChoixEnchereVuesModele : BaseVueModele
     {
         private readonly Api _apiServices = new Api();
 
-        
+
 
         private ObservableCollection<Enchere> _enchereList;
+        private ObservableCollection<Enchere> _enchereListClassique;
+
+
 
         public ObservableCollection<Enchere> EnchereList
         {
             get { return _enchereList; }
             set { SetProperty(ref _enchereList, value); }
         }
+        public ObservableCollection<Enchere> EnchereClassiqueList
+        {
+            get { return _enchereListClassique; }
+            set{  SetProperty(ref _enchereListClassique, value);}
+            
+        }
+
 
 
         public PageChoixEnchereVuesModele()
@@ -30,9 +41,9 @@ namespace ApS2.VueModele
 
         }
 
-        public async Task<ObservableCollection<Enchere>>GetEnchereEnCoursId(int id)
+        public async Task<ObservableCollection<Enchere>> GetEnchereTypeEnCours(int letype)
         {
-            var x =await _apiServices.GetAllAsyncID("api/getEncheresEnCours", Enchere.collClass, "Id", id);
+            var x = await _apiServices.GetAllAsyncID("api/getEncheresEnCours", Enchere.collClass, "IdTypeEnchere", letype);
             Enchere.collClass.Clear();
             return x;
         }
@@ -42,11 +53,18 @@ namespace ApS2.VueModele
             Enchere.collClass.Clear();
             return x;
         }
-        public async void Init ()
+        public async void Init()
         {
-            EnchereList = await GetEnchereEnCours();
+            EnchereClassiqueList = await GetEnchereTypeEnCours(1);
             
+
         }
+
+        
+            
+    
+         
+        
 
 
     }
